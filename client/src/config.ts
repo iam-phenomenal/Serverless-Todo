099c4ac6b535f6d2d18598790d8d1ed9eb6e8351
iam-phenomenal <ayandiranibrahim@outlook.com>
2023-03-25 02:14:03 +0100

    Initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'hx9ybpvsp4';
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`;
+
+export const authConfig = {
+  // Auth0 settings
+  domain: 'dev-jyq4bnwu.eu.auth0.com',
+  clientId: 'RT3Br26wdKLR2hyVDIpDgTIG4hLwLo59',
+  callbackUrl: 'http://localhost:3000/callback'
+};
