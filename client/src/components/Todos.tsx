6b641a98814bcee8172bf75425b486dfa9f5b049
iam-phenomenal <ayandiranibrahim@outlook.com>
2023-03-25 14:31:00 +0100

    style: Starter code

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -1,7 +1,7 @@
-import dateFormat from 'dateformat';
-import { History } from 'history';
-import update from 'immutability-helper';
-import * as React from 'react';
+import dateFormat from 'dateformat'
+import { History } from 'history'
+import update from 'immutability-helper'
+import * as React from 'react'
 import {
   Button,
   Checkbox,
@@ -12,93 +12,94 @@ import {
   Input,
   Image,
   Loader
-} from 'semantic-ui-react';
+} from 'semantic-ui-react'
 
-import { createTodo, deleteTodo, getTodos, updateTodo } from '../api/todos-api';
-import Auth from '../auth/Auth';
-import { TodoItem } from '../types/Todo.d';
+import { createTodo, deleteTodo, getTodos, patchTodo } from '../api/todos-api'
+import Auth from '../auth/Auth'
+import { Todo } from '../types/Todo'
 
 interface TodosProps {
-  auth: Auth;
-  history: History;
+  auth: Auth
+  history: History
 }
+
 interface TodosState {
-  todos: TodoItem[];
-  newTodoName: string;
-  loadingTodos: boolean;
+  todos: Todo[]
+  newTodoName: string
+  loadingTodos: boolean
 }
+
 export class Todos extends React.PureComponent<TodosProps, TodosState> {
   state: TodosState = {
     todos: [],
     newTodoName: '',
     loadingTodos: true
-  };
-
-  async componentDidMount() {
-    try {
-      const todos = await getTodos(this.props.auth.getIdToken());
-      this.setState({
-        todos,
-        loadingTodos: false
-      });
-    } catch (error) {
-      console.error(`Failed to fetch todos: ${error.message}`);
-    }
   }
 
   handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ newTodoName: event.target.value });
-  };
+    this.setState({ newTodoName: event.target.value })
+  }
 
   onEditButtonClick = (todoId: string) => {
-    this.props.history.push(`/todos/${todoId}/edit`);
-  };
+    this.props.history.push(`/todos/${todoId}/edit`)
+  }
 
   onTodoCreate = async (event: React.ChangeEvent<HTMLButtonElement>) => {
     try {
-      const dueDate = this.calculateDueDate();
+      const dueDate = this.calculateDueDate()
       const newTodo = await createTodo(this.props.auth.getIdToken(), {
         name: this.state.newTodoName,
-        dueDate,
-        attachmentUrl: ''
-      });
+        dueDate
+      })
       this.setState({
         todos: [...this.state.todos, newTodo],
         newTodoName: ''
-      });
+      })
     } catch {
-      alert('Todo creation failed');
+      alert('Todo creation failed')
     }
-  };
+  }
 
-  onTodoDelete = async (todoId: string, createdAt: string) => {
+  onTodoDelete = async (todoId: string) => {
     try {
-      await deleteTodo(this.props.auth.getIdToken(), todoId);
+      await deleteTodo(this.props.auth.getIdToken(), todoId)
       this.setState({
-        todos: this.state.todos.filter((todo) => todo.todoId !== todoId)
-      });
+        todos: this.state.todos.filter(todo => todo.todoId !== todoId)
+      })
     } catch {
-      alert('Todo deletion failed');
+      alert('Todo deletion failed')
     }
-  };
+  }
 
   onTodoCheck = async (pos: number) => {
     try {
-      const todo = this.state.todos[pos];
-      await updateTodo(this.props.auth.getIdToken(), todo.todoId, {
+      const todo = this.state.todos[pos]
+      await patchTodo(this.props.auth.getIdToken(), todo.todoId, {
         name: todo.name,
         dueDate: todo.dueDate,
         done: !todo.done
-      });
+      })
       this.setState({
         todos: update(this.state.todos, {
           [pos]: { done: { $set: !todo.done } }
         })
-      });
+      })
     } catch {
-      alert('Todo deletion failed');
+      alert('Todo deletion failed')
     }
-  };
+  }
+
+  async componentDidMount() {
+    try {
+      const todos = await getTodos(this.props.auth.getIdToken())
+      this.setState({
+        todos,
+        loadingTodos: false
+      })
+    } catch (e) {
+      alert(`Failed to fetch todos: ${(e as Error).message}`)
+    }
+  }
 
   render() {
     return (
@@ -109,7 +110,7 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
 
         {this.renderTodos()}
       </div>
-    );
+    )
   }
 
   renderCreateTodoInput() {
@@ -134,15 +135,15 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
           <Divider />
         </Grid.Column>
       </Grid.Row>
-    );
+    )
   }
 
   renderTodos() {
     if (this.state.loadingTodos) {
-      return this.renderLoading();
+      return this.renderLoading()
     }
 
-    return this.renderTodosList();
+    return this.renderTodosList()
   }
 
   renderLoading() {
@@ -152,7 +153,7 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
           Loading TODOs
         </Loader>
       </Grid.Row>
-    );
+    )
   }
 
   renderTodosList() {
@@ -162,7 +163,10 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
           return (
             <Grid.Row key={todo.todoId}>
               <Grid.Column width={1} verticalAlign="middle">
-                <Checkbox onChange={() => this.onTodoCheck(pos)} checked={todo.done} />
+                <Checkbox
+                  onChange={() => this.onTodoCheck(pos)}
+                  checked={todo.done}
+                />
               </Grid.Column>
               <Grid.Column width={10} verticalAlign="middle">
                 {todo.name}
@@ -171,7 +175,11 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
                 {todo.dueDate}
               </Grid.Column>
               <Grid.Column width={1} floated="right">
-                <Button icon color="blue" onClick={() => this.onEditButtonClick(todo.todoId)}>
+                <Button
+                  icon
+                  color="blue"
+                  onClick={() => this.onEditButtonClick(todo.todoId)}
+                >
                   <Icon name="pencil" />
                 </Button>
               </Grid.Column>
@@ -179,26 +187,28 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
                 <Button
                   icon
                   color="red"
-                  onClick={() => this.onTodoDelete(todo.todoId, todo.createdAt)}
+                  onClick={() => this.onTodoDelete(todo.todoId)}
                 >
                   <Icon name="delete" />
                 </Button>
               </Grid.Column>
-              {todo.attachmentUrl && <Image src={todo.attachmentUrl} size="small" wrapped />}
+              {todo.attachmentUrl && (
+                <Image src={todo.attachmentUrl} size="small" wrapped />
+              )}
               <Grid.Column width={16}>
                 <Divider />
               </Grid.Column>
             </Grid.Row>
-          );
+          )
         })}
       </Grid>
-    );
+    )
   }
 
   calculateDueDate(): string {
-    const date = new Date();
-    date.setDate(date.getDate() + 7);
+    const date = new Date()
+    date.setDate(date.getDate() + 7)
 
-    return dateFormat(date, 'yyyy-mm-dd') as string;
+    return dateFormat(date, 'yyyy-mm-dd') as string
   }
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,11 @@
-const apiId = 'hx9ybpvsp4';
-export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`;
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'g5l6z3lcn6'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // Auth0 settings
-  domain: 'dev-jyq4bnwu.eu.auth0.com',
-  clientId: 'RT3Br26wdKLR2hyVDIpDgTIG4hLwLo59',
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-lyzqwymoiy2vjmq0.us.auth0.com',            // Auth0 domain
+  clientId: 'lqBPPq7ztHd7ceBKEKa1PKcgoMqOMp85',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
-};
+}
